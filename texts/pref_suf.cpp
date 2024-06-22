f4c001dabb2b7329e553cdf8913d3c0ca5168bee
annaszymanska1205 <annamaria.szymanska@student.uj.edu.pl>
2024-06-22 14:24:38 +0200

    Texts update

---
+++ texts/pref_suf.cpp
@@ -0,0 +1,60 @@
+/**
+ * The LPS array and Knuth-Morris-Pratt algorithm
+ * for pattern searching in text.
+ * Time complexity: O(|text| + |pattern|)
+ * Space complexity: O(|text|)
+ * Created on 22.06.2024
+ */
+
+#include <vector>
+
+/**
+ * Get the longest prefix-suffix array
+ * @param txt       text as array
+ * @return          array with length of maximum proper prefix
+ *                  that is also the suffix of txt[0...i-1]
+ */
+std::vector<int> get_lps(const std::vector<char>& txt) {
+    int n = static_cast<int>(txt.size());
+    std::vector<int> P(n+1);
+    int t;
+    t = P[0] = -1;
+    for (int i = 0; i < n; i++) {
+        while (t != -1 && txt[t] != txt[i]) {
+            t = P[t];
+        }
+        P[i + 1] = ++t;
+    }
+    return P;
+}
+
+/**
+ * Find all occurrences of pattern in text
+ * @param pat       patter as array
+ * @param txt       text as array
+ * @return          starting positions of all found occurrences
+ */
+std::vector<int> kmp(const std::vector<char> & pat, const std::vector<char> & txt) {
+    int n = static_cast<int>(txt.size());
+    int m = static_cast<int>(pat.size());
+    std::vector<int> matches;
+    auto lps_pat = get_lps(pat);
+
+    int i = 0, j = 0;
+    while (i - j <= n - m) {
+        if (pat[j] == txt[i]) {
+            i++; j++;
+        }
+        if (j == m) {
+            matches.push_back(i - j);
+            j = lps_pat[j];
+        } else if (i < n && pat[j] != txt[i]) {
+            j = lps_pat[j];
+            if (j < 0) {
+                i++; j++;
+            }
+        }
+    }
+    return matches;
+}
+
--- texts/test.cpp
+++ texts/test.cpp
@@ -1,18 +1,34 @@
-#include "aho_corasick_tree.h"
+#include "pref_suf.cpp"
 #include <iostream>
 #include <cassert>
 
-void test_aho_corasick() {
-    std::vector<std::string> patterns = {"berry", "mango", "pear", "peach"};
-    AhoCorasickTree Tree(patterns);
-    // Check if given texts contain any of the patterns
-    assert(Tree.find("raspberry") == true);
-    assert(Tree.find("pearl") == true);
-    assert(Tree.find("apple pie") == false);
-    std::cout << "Aho-Corasick tree test: OK" << std::endl;
+void test_lps() {
+    std::vector<char> txt0 = {'a', 'a', 'a', 'a'};
+    std::vector<int> pref_suf0 = {-1, 0, 1, 2, 3};
+    assert(pref_suf0 == get_lps(txt0));
+
+    std::vector<char> txt1 = {'a', 'b', 'c', 'a', 'b', 'a', 'd'};
+    std::vector<int> pref_suf1 = {-1, 0, 0, 0, 1, 2, 1, 0};
+    assert(pref_suf1 == get_lps(txt1));
+
+    std::vector<char> txt2 = {};
+    std::vector<int> pref_suf2 = {-1};
+    assert(pref_suf2 == get_lps(txt2));
+
+    std::cout << "Prefix-suffix test: OK" << std::endl;
+}
+
+void test_kmp() {
+    std::vector<char> pat = {'a', 'b'};
+    std::vector<char> txt = {'a', 'b', 'a', 'a', 'b', 'a', 'd'};
+    std::vector<int> matches = {0, 3};
+    assert(matches == kmp(pat, txt));
+
+    std::cout << "KMP test: OK" << std::endl;
 }
 
 int main() {
-    test_aho_corasick();
+    test_lps();
+    test_kmp();
     return 0;
 }
